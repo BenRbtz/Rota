4fdb87633c102feaf274afb508a5e11f0a545f2f
Ben <benroberts96@hotmail.co.uk>
2019-03-01 00:22:55 +0000

    Refactor entrypoint to use args
    can provide file_name, year, month_name, and day_name to entrypoint
    script and it will be used to generate the rota

--- example_script.py
+++ example_script.py
@@ -1,7 +1,21 @@
+import argparse
+import datetime
+
 from generate import calender, spreadsheet, rota
+
+current_year = datetime.datetime.now().year
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-f', '--file_name', help="file name to store rota output", default='rota.xlsx')
+parser.add_argument('-y', '--year', help="Desired year for rota", type=int, default=current_year)
+parser.add_argument('-m', '--month_name', help="Name of the month desired for rota", required=True)
+parser.add_argument('-d', '--day_names', help="Names of days desired for rota", required=True, nargs='+')
+
+args = parser.parse_args()
 
 my_spreadsheet = spreadsheet.Spreadsheet()
 my_calender = calender.Calender()
 my_rota = rota.Rota(my_calender, my_spreadsheet)
 
-my_rota.run('rota.xlsx', 2019, 'February', 'Tuesday', 'Friday')
+my_rota.run(args.file_name, args.year, args.month_name, args.day_names)
